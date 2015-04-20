850c06a42fcbf33f2e717f5e8bcbddc4a5e8f0bc
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2015-04-20 01:23:26 +0000

    Unifying whitespace.

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,12 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.vprog;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-                config.title = "VProg2D";
-                config.height = 720;
-                config.width = 1280;
-                config.resizable = false;
-		new LwjglApplication(new vprog(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "VProg2D";
+        config.height = 720;
+        config.width = 1280;
+        config.resizable = false;
+        new LwjglApplication(new vprog(), config);
+    }
 }
