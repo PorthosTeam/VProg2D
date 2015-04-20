1e4f1eb4979a1993a3888b7344a07fd7ed5df6a0
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2015-04-20 02:24:19 +0000

    Playing around with creating a basic Swing UI window.

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,8 +4,26 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.vprog;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class DesktopLauncher {
+    // Swing is not thread safe, so it is "good practice" to move all Swing
+    // commands out of main, into a function that is run by the event dispatcher
+    // thread once it's actually safe.
+    public static void startSwingGUI() {
+        JFrame testJFrame = new JFrame("Test JFrame");
+        testJFrame.setBounds(0, 0, 200, 50);
+        testJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        testJFrame.setVisible(true);
+    }
+    
     public static void main (String[] arg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                startSwingGUI();
+            }
+        });
         LwjglApplicationConfiguration config
             = new LwjglApplicationConfiguration();
         config.title = "VProg2D";
