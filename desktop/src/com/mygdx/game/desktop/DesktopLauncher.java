869fd003836fe609b5c655b2702f002df840ee83
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2015-04-20 04:49:51 +0000

    Slightly cleaner refactor of prior example.

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -21,22 +21,30 @@ public class DesktopLauncher {
     // thread once it's actually safe.
     private static void startSwingGUI() {
         // Don't want any of this fixed-size stuff if we can avoid it, so we get
-        // the screen area available to us. First by grabbing the overall screen
-        // graphics configuration...
-        GraphicsConfiguration graphicsConf
-            = GraphicsEnvironment.getLocalGraphicsEnvironment()
-              .getDefaultScreenDevice().getDefaultConfiguration();
-        // ..then by grabbing the 'real' screen bounds (e.g. physical)...
-        Rectangle screenBounds = getRealScreenBounds(graphicsConf);
-        // ..then getting the "insets"...
-        Insets screenInsets = getScreenInsets(graphicsConf);
-        // ..and finally calculating the effective screen bounds based on the
-        // physical bounds, minus the insets.
-	screenBounds = calculateInsetBounds(screenBounds, screenInsets);
+        // the screen area available to us for our windows (we don't want to
+        // open windows bigger than these bounds if we can avoid it).
+	Rectangle screenBounds = getWindowBounds();
         openTestWindow(screenBounds);
     }
     
-    private static Rectangle getRealScreenBounds
+    private static Rectangle getWindowBounds() {
+        // First grab the overall screen graphics configuration...
+        GraphicsConfiguration graphicsConf
+            = GraphicsEnvironment.getLocalGraphicsEnvironment()
+              .getDefaultScreenDevice().getDefaultConfiguration();
+	
+        // ..then by grabbing the 'real' screen bounds (e.g. physical)...
+        Rectangle screenBounds = getScreenBounds(graphicsConf);
+        
+        // ..then getting the "insets"...
+        Insets screenInsets = getScreenInsets(graphicsConf);
+        
+        // ..and finally calculating the effective screen bounds based on the
+        // physical bounds, minus the insets.
+	return calculateInsetBounds(screenBounds, screenInsets);
+    }
+    
+    private static Rectangle getScreenBounds
     (GraphicsConfiguration graphicsConf) {
         return graphicsConf.getBounds();
     }
@@ -48,23 +56,23 @@ public class DesktopLauncher {
     private static Rectangle calculateInsetBounds
     (Rectangle bounds, Insets margin) {
         Rectangle insetBounds = new Rectangle();
-	insetBounds.x = bounds.x + margin.left;
-	insetBounds.y = bounds.y + margin.top;
-	insetBounds.width = bounds.width - margin.left - margin.right;
-	insetBounds.height = bounds.height - margin.top - margin.bottom;
-	return insetBounds;
+        insetBounds.x = bounds.x + margin.left;
+        insetBounds.y = bounds.y + margin.top;
+        insetBounds.width = bounds.width - margin.left - margin.right;
+        insetBounds.height = bounds.height - margin.top - margin.bottom;
+        return insetBounds;
     }
     
     private static void openTestWindow(Rectangle bounds) {
         String labelText = bounds.x + " " + bounds.y + " " + bounds.width + " "
             + bounds.height;
         JLabel testJLabel = new JLabel(labelText);
-	
+        
         JFrame testJFrame = new JFrame("Test JFrame");
         testJFrame.setBounds(bounds);
         testJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	testJFrame.getContentPane().add(testJLabel);
+        
+        testJFrame.getContentPane().add(testJLabel);
         testJFrame.setVisible(true);
     }
     
